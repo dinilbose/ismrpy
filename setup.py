71eabc5db55d6b7bcd5b673d59aeabf2103e1036
dinilbose <dinilbose@gmail.com>
2019-10-12 23:51:14 +0530

    intial

---
+++ setup.py
@@ -0,0 +1,26 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+  name = 'ismrpy',         # How you named your package folder (MyLib)
+  packages = ['ismrpy'],   # Chose the same as "name"
+  version = '0.2.5',      # Start with a small number and increase it with every change you make
+  license='gpl-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Reads ismr files and provides pandas data frame',   # Give a short description about your library
+  author = 'Dinil Bose P',                   # Type in your name
+  author_email = 'dinilbose@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/dinilbose/ismrpy',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/dinilbose/ismrpy/archive/v0.1-beta.tar.gz',    # I explain this later on
+  keywords = ['ismr', 'pandas'],   # Keywords that define your package best
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Science/Research',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ], install_requires=['pandas>=0.13.1'],setup_requires=["numpy","pandas"],
+
+)
